3f30012c01dd7f852ddb822ba9473b8800f846b9
BhanuGonugunta <bhanugonugunta@gmail.com>
2022-06-07 11:13:15 +0530

    Initial Commit

---
+++ BusinessLoan.Entities/Document.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLoan.Entities
+{
+    class Document
+    {
+        public int DocumentId { get; set; }
+        public string DocumentType { get; set; }
+        public string DocumentUpload { get; set; }
+    }
+}
---
+++ BusinessLoan.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BusinessLoan.UI.MvcApplication" Language="C#" %>
